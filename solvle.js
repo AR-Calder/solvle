59a625d0fb02a41e37be033fd3c226b49bb7433f
Andrew Calder <Androbcal@gmail.com>
2022-01-11 13:20:34 +0000

    Update solvle.js

--- solvle.js
+++ solvle.js
@@ -13,10 +13,10 @@ const userInput = (query) => {
     }))
 }
 
-const removeWordsContainingBadChars = (wordlist, guess, posCorrectChars, correctChars) => {
-    let allCorrect = posCorrectChars + correctChars
+const removeWordsContainingBadChars = (wordlist, guess, greenChars, yellowChars) => {
+    let correctChars = greenChars + yellowChars
     for (const char of guess) {
-        if (!allCorrect.includes(char)){
+        if (!correctChars.includes(char)){
             // character does not exist in word
             wordlist = wordlist.filter(word => !word.includes(char))
         }
@@ -24,7 +24,8 @@ const removeWordsContainingBadChars = (wordlist, guess, posCorrectChars, correct
     return wordlist
 }
 
-const removeWordsMissingKnownChars = (wordlist, correctChars) => {
+const removeWordsMissingKnownChars = (wordlist, greenChars, yellowChars) => {
+    let correctChars = greenChars + yellowChars
     for (const char of correctChars){
         wordlist = wordlist.filter(word => word.includes(char))
     }
@@ -37,15 +38,15 @@ const charPositions = (str, char) => {
     return tmp.reduce((res, elem, idx) => elem.toLowerCase() === char ? [...res, idx] : res, [])
 }
 
-const removeWordsContainingBadPosChars = (wordlist, guess, posCorrectChars, correctChars) => {
-    for (const char of correctChars){
-        if (posCorrectChars.includes(char)){
-            // remove words which do not use known character position
-            wordlist = wordlist.filter(word => charPositions(word, char).some(pos => charPositions(guess, char).includes(pos)))      
-        } else {
-            // remove words which contain known characters in known incorrect positions
-            wordlist = wordlist.filter(word => !charPositions(word, char).some(pos => charPositions(guess, char).includes(pos)))
-        }
+const removeWordsContainingBadPosChars = (wordlist, guess, greenChars, yellowChars) => {
+    // remove words which do not use known character position
+    for (const char of greenChars){
+        wordlist = wordlist.filter(word => charPositions(word, char).some(pos => charPositions(guess, char).includes(pos))) 
+    }
+    
+    // remove words which contain known characters in known incorrect positions
+    for (const char of yellowChars){
+        wordlist = wordlist.filter(word => !charPositions(word, char).some(pos => charPositions(guess, char).includes(pos)))
     }
 
     return wordlist
@@ -78,36 +79,43 @@ const suggestedWords = (wordlist) => {
 }
 
 async function main(){
-    let guess = ""
-    let found = false
     let wordlist = dictionary.wordlist
-    while (false === found){
+    let status = ""
+    while (status === ""){
         
-        guess = await userInput("guess?: ")
+        let guess = await userInput("guess?: ")
 
-        const correctChars = await userInput("Which (if any) of the guess characters are correct?: ")
+        const yellowChars = await userInput("Which (if any) of the guess characters are yellow?: ")
 
-        const posCorrectChars = await userInput("Which (if any) of the guess characters are correct AND in the correct position?: ")
-        if (posCorrectChars.length === 5) break
+        const greenChars = await userInput("Which (if any) of the guess characters are green?: ")
+        if (greenChars.length === 5){
+            status = "Success!"
+            continue
+        }
 
         // only include words which use known characters
-        wordlist = removeWordsMissingKnownChars(wordlist, correctChars)
+        wordlist = removeWordsMissingKnownChars(wordlist, greenChars, yellowChars)
 
         // exlude words that use known bad characters
-        wordlist = removeWordsContainingBadChars(wordlist, guess, posCorrectChars, correctChars)
+        wordlist = removeWordsContainingBadChars(wordlist, guess, greenChars, yellowChars)
 
         // only include words matching known character positions
-        wordlist = removeWordsContainingBadPosChars(wordlist, guess, posCorrectChars,correctChars)
+        wordlist = removeWordsContainingBadPosChars(wordlist, guess, greenChars, yellowChars)
 
         const suggestions = suggestedWords(wordlist)
         if (suggestions.length > 5){
             suggestions.length = 5
         }
         console.log('recommended words:', suggestions.join(', '))
-        if (wordlist.length < 2) break
+        if (wordlist.length == 1) {
+            status = "Success!"
+        }
+        if (wordlist.length == 0) {
+            status = "No matching word in dictionary or typo in previous answer!"
+        }
         console.log() 
     }
-    console.log("Success!")
+    console.log(status)
 }
 
 main()
